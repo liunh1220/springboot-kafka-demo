8eb2c18b4c7862645f96acd153272b400479bd1b
liunanhua <liunanhua@chinasie.com>
2018-12-04 13:34:03 +0800

    fix

--- springboot-kafka-producer-demo/src/main/java/com/example/demo/service/KafkaSender.java
+++ springboot-kafka-producer-demo/src/main/java/com/example/demo/service/KafkaSender.java
@@ -1,6 +1,9 @@
 package com.example.demo.service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo.config.Globals;
+import com.example.demo.model.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -35,7 +39,8 @@ public class KafkaSender {
             /*Message message = new Message();
             message.setId(uuid);
             message.setMsg(msg);
-            message.setSendTime(new Date());*/
+            message.setSendTime(new Date());
+            String jsonMsg = JSONObject.toJSONString(message);*/
 
             kafkaTemplate.send(topic, uuid, msg);
 
